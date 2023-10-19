1ebcdc0027c35acbfd99b1298a1ae723332e38ea
nneliifeanyi <stanvicbest@gmail.com>
2023-10-19 15:18:32 +0100

    con

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "stanvicc_GLAS");
-define("DB_PASS", "GloryLandAcademy");
+define("DB_USER", "stanvicc_cpm2");
+define("DB_PASS", "gloryLandAcademy");
 define("DB_NAME", "stanvicc_Cpm");
